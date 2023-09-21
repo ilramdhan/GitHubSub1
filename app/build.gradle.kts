6579250b00064cced13c6fd1fbe483b476185c19
Ilham Ramadhan <ilhamram332@gmail.com>
2023-09-22 00:29:23 +0700

    setup & search user done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("com.square.retrofit2:retrofit:2.9.0")
-    implementation("com.square.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.google.android.material:material:1.9.0")
 
